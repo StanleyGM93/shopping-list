c323bd9aa1dce049836eca4d82bd06e7c6977a34
Stanley Mair <stanleygmair@gmail.com>
2023-08-29 15:51:15 +1200

    Added CRD routes

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,12 @@
 import * as Path from 'node:path'
-
 import express from 'express'
 
+import listRouter from './routes/list.ts'
 
 const server = express()
 server.use(express.json())
+
+server.use('/', listRouter)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
