e49d670b8ee60ea26912c8b2ef9a1f15679fac61
Stanley Mair <stanleygmair@gmail.com>
2023-08-30 10:40:07 +1200

    Fixed CRD funcs on components

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,16 +1,17 @@
 import request from 'superagent'
 import { NewListItem, ListItem } from '../../models/List'
 
+const url = '/api/v1/items'
+
 export async function getAllListItems() {
-  const response = await request.get('/api/v1/items')
-  console.log(response.body)
+  const response = await request.get(url)
   return response.body as ListItem[]
 }
 
 export async function addListItem(listItem: NewListItem): Promise<void> {
-  await request.post('/').send(listItem)
+  await request.post(url).send(listItem)
 }
 
 export async function deleteListItem(id: number): Promise<void> {
-  await request.delete(`/${id}`)
+  await request.delete(`${url}/${id}`)
 }
--- client/components/NewItemForm.tsx
+++ client/components/NewItemForm.tsx
@@ -47,6 +47,7 @@ export default function NewItemForm() {
           onChange={handleChange}
         />
       </div>
+      <button type="submit">Add item</button>
     </form>
   )
 }
